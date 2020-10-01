db3f11339ce0254fbcd88acc0fb678cc06b4b547
Ricardo Ogliari <ricardo.ogliari@gok.digital>
2020-09-30 22:08:25 -0300

    leitura dos dados e exclusão de dados no Firebase

--- lib/firebase/listCar.dart
+++ lib/firebase/listCar.dart
@@ -37,8 +37,45 @@ class ListCars extends StatelessWidget {
   }
 
   Widget _buildBody(BuildContext context) {
-    return Center();
+    return StreamBuilder<QuerySnapshot>(
+      stream: Firestore.instance.collection('car').snapshots(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) return LinearProgressIndicator();
+
+        return _buildList(context, snapshot.data.documents);
+      },
+    );
   }
+
+  Widget _buildList(BuildContext context, List<DocumentSnapshot> snapshot) {
+    return ListView(
+      padding: const EdgeInsets.only(top: 20.0),
+      children: snapshot.map((data) => _buildListItem(context, data)).toList(),
+    );
+  }
+
+  Widget _buildListItem(BuildContext context, DocumentSnapshot data) {
+    final record = Car.fromSnapshot(data);
+
+    return Padding(
+      key: ValueKey(record.name),
+      padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.grey),
+          borderRadius: BorderRadius.circular(5.0),
+        ),
+        child: ListTile(
+          title: Text(record.name),
+          trailing: Text(record.brand),
+          onLongPress: (){
+            data.reference.delete();
+          },
+        ),
+      ),
+    );
+  }
+
 
 
 }
